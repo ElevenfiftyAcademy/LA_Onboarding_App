1f9e96da6f2847b5769c1a65cf28087283d94b6a
Zach Maynard <maynard.zach23@gmail.com>
2020-10-26 16:16:50 -0400

    debug complete

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 function displayEvenNumbers() {
     let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
     let evenNumbers = [];
-    for (let i = 0; i < numbers.length; i++;) {
+    for (let i = 0; i < numbers.length; i++) {
         if (numbers[i] % 2 === 0); {
             evenNumbers.push(numbers[i]);
         }
